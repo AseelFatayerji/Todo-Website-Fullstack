e32c37ad8e46e6031423abbf6f81f991a2a82fad
Nyx-Nightmare <assel.fatayerji201@gmail.com>
2024-02-29 01:42:43 +0200

    Second commit

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 let count = 0;
+window.onload = function() {
+    if ( sessionStorage.getItem('user') == "user" && sessionStorage.getItem('count') == "count"){
+        count = 0
+        return
+    }
+    console.log(sessionStorage.getItem('user'));
+    //document.getElementById('list').innerHTML = sessionStorage.getItem('user');
+    //count += parseFloat(sessionStorage.getItem('count'));
+    console.log(sessionStorage.getItem('count'));
+}
+function removeitem(id) {
+    let list = document.getElementById(id);
+    console.log(id)
+    list.remove();
+    count--;
+  };
 
 function checkinfo() {
   let user = document.getElementById("user").value;
@@ -35,20 +51,21 @@ function additem() {
   bullet.className = "fa-regular fa-circle-dot item";
 
   node.className ="text"
+  console.log(count)
   node.id = count;
   check.id = count;
   count++;
   check.type = "checkbox";
   check.className = "checkbox-round";
-  check.addEventListener("click", function handleClick() {
+  check.onclick = function () {
     let list = document.getElementById(this.id);
-    list.remove();
-  });
+    removeitem(this.id)
+  };
   node.appendChild(bullet)
   node.appendChild(document.createTextNode(item));
   node.appendChild(check);
   document.getElementById("list").appendChild(node);
-  
+
   if (document.getElementById("dates").checked) {
     let dates = document.createElement("INPUT");
     dates.setAttribute("type", "date");
@@ -59,9 +76,11 @@ function additem() {
     let alert = document.createElement("i");
     alert.className = "fa-solid fa-circle-exclamation item";
     node.className = "alerttext";
-    node.replace(alert, node.firstChild)
+    node.replaceChild(alert, node.firstChild);
 
   }
-
-
+    node.appendChild(check);
+    document.getElementById("list").appendChild(node);
+    sessionStorage.setItem('user', document.getElementById("list").innerHTML)
+    sessionStorage.setItem('count',count)
 }
